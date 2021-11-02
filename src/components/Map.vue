7972118907e3b66bbba89f5220a23d0f27c74e05
EvgeniyaNesterkova <miledieva08@gmail.com>
2021-11-02 23:20:35 +0300

    Основное задание

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,55 +1,115 @@
 <template>
-    <div class="map">
-        <h3>Карта офиса</h3>
+  <div class="map">
+    <h3>Карта офиса</h3>
 
-        <div
-            v-if="!isLoading"
-            class="map-root"
-        >
-            <!-- map -->
-        </div>
-        <div v-else>Loading...</div>
+    <div v-if="!isLoading" class="map-root">
+      <MapSVG ref="svg" />
+      <Table v-show="false" ref="table" />
     </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script>
+import MapSVG from "@/assets/images/map.svg";
+import tables from "@/assets/data/tables.json";
+import Table from "@/assets/images/workPlace.svg";
+import legend from "@/assets/data/legend.json";
+import * as d3 from "d3";
+
 export default {
-    data() {
-        return {
-            isLoading: false,
-        };
+  data() {
+    return {
+      isLoading: false,
+      svg: null,
+      g: null,
+      tableSVG: null,
+      tables: [],
+    };
+  },
+  components: {
+    MapSVG,
+    Table,
+  },
+  mounted() {
+    this.$nextTick(function () {
+      this.isLoading = true;
+
+      this.svg = d3.select(this.$refs.svg);
+      this.g = this.svg.select("g");
+      this.tableSVG = d3.select(this.$refs.table);
+
+      if (!this.g.empty()) {
+        this.drawTables();
+      } else {
+        alert("SVG is incorrect");
+      }
+
+      this.isLoading = false;
+    });
+  },
+  created() {
+    this.tables = tables;
+  },
+  methods: {
+    drawTables() {
+      const svgTablesGroupPlace = this.g
+        .append("g")
+        .classed("groupPlaces", true);
+
+      this.tables.forEach((table) => {
+        const targetSeat = svgTablesGroupPlace
+          .append("g")
+          .attr("transform", `translate(${table.x}, ${table.y}) scale(0.5)`)
+          .attr("id", table._id)
+          .classed("employer-place", true);
+
+        targetSeat
+          .append("g")
+          .attr("transform", `rotate(${table.rotate || 0})`)
+          .attr("group_id", table.group_id)
+          .classed("table", true)
+          .html(this.tableSVG.html())
+          .attr(
+            // устанавливаем цвет подразделения
+            "fill",
+            legend.find((it) => it.group_id === table.group_id)?.color ??
+              "transparent"
+          );
+      });
     },
+  },
 };
 </script>
 
 <style scoped>
 .map {
-    height: 100%;
-    width: 100%;
-    padding: 24px;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 24px;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 }
 
 .map-root {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 ::v-deep svg {
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 }
 
 ::v-deep .table {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
